6e6bf4778fec950f0bf908f7a4e6582b06e555d1
Marat Mingazov <mingazov.marat2003@yandex.ru>
2022-06-01 14:02:22 +0300

    time range set, comments deleted

--- scripts/autodownloader.py
+++ scripts/autodownloader.py
@@ -1,10 +1,7 @@
-from scripts.tables_downloader import start_up, download_du_active, download_request_active  # , download_raskraska
-# from scripts.timetable_editor import get_edit_timetable
+from scripts.tables_downloader import start_up, download_du_active, download_request_active
 from handlers.activity import update_activity
 from threading import Thread
 from time import sleep, strftime
-
-# sheet = {'1 курс': '1course', '2 курс': '2course', '3 курс': '3course', '4 курс': '4course', 'Магистры': 'masters'}
 
 
 def download():
@@ -13,15 +10,11 @@ def download():
     print(f'{strftime("%d-%m-%Y %H:%M:%S")}: du downloaded successfully')
     download_request_active()
     print(f'{strftime("%d-%m-%Y %H:%M:%S")}: request downloaded successfully')
-    # download_raskraska()
-    # print(f'{strftime("%d-%m-%Y %H:%M:%S")}: timetable downloaded successfully')
-    # get_edit_timetable(sheet)
-    # print(f'{strftime("%d-%m-%Y %H:%M:%S")}: timetable edited successfully')
 
 
 def run_download():
     while True:
-        sleep(100)
+        sleep(28000)  # треть дня
         download()
         sleep(60)
         update_activity()
